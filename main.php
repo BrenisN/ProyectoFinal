f7450f792721df5f8aa1336dfc2e3cf8a49f7b43
HwaRCala <leo.lviola@gmail.com>
2023-11-01 19:29:42 -0300

    leo01112023

---
+++ main.php
@@ -0,0 +1,154 @@
+<?php
+require "secciones.php";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./ECOMMERCE.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap"
+    />
+  </head>
+  <body>
+    <div class="ecommerce">
+      <div class="footer21">
+        <div class="iconos22">
+          <div class="footer22"></div>
+          <img
+            class="logo-registrado-icon22"
+            alt=""
+            src="./public/logo-registrado@2x.png"
+          />
+
+          <div class="repar22">REP.AR</div>
+          <img
+            class="logo-facebook-icon22"
+            alt=""
+            src="./public/logo-facebook@2x.png"
+          />
+
+          <img class="logo-ig-icon22" alt="" src="./public/logo-ig@2x.png" />
+
+          <img
+            class="logo-twitter-icon22"
+            alt=""
+            src="./public/logo-twitter@2x.png"
+          />
+        </div>
+      </div>
+      <div class="body19">
+        <div class="body20"></div>
+        <div class="side-panel">
+          <div class="form14"></div>
+          <div class="categorias">
+            <div class="categorias-child"></div>
+            <div class="varios">Varios</div>
+            <img class="separador-icon" alt="" src="./public/separador.svg" />
+
+            <div class="mecanica">Mecánica</div>
+            <img class="separador-icon1" alt="" src="./public/separador1.svg" />
+
+            <div class="electro">Electro</div>
+            <img class="separador-icon2" alt="" src="./public/separador2.svg" />
+
+            <div class="refrigeracion">Refrigeriacion</div>
+            <img class="separador-icon3" alt="" src="./public/separador3.svg" />
+
+            <div class="gas">GAS</div>
+            <img class="separador-icon4" alt="" src="./public/separador4.svg" />
+
+            <div class="electricidad">Electricidad</div>
+            <img class="separador-icon5" alt="" src="./public/separador5.svg" />
+
+            <div class="heramientas">Herramientas</div>
+            <img class="separador-icon6" alt="" src="./public/separador6.svg" />
+
+            <div class="plomeria">Plomeria</div>
+            <img class="separador-icon7" alt="" src="./public/separador7.svg" />
+
+            <div class="carpinteria">Carpinteria</div>
+            <img class="separador-icon8" alt="" src="./public/separador8.svg" />
+
+            <div class="pintura">Pintura</div>
+            <img class="separador-icon9" alt="" src="./public/separador9.svg" />
+
+            <div class="construccion">Construccion</div>
+            <img
+              class="separador-icon10"
+              alt=""
+              src="./public/separador10.svg"
+            />
+
+            <div class="categorias1">Categorias</div>
+          </div>
+        </div>
+      </div>
+      <div class="nav42">
+        <div class="nav43"></div>
+        <div class="registro70" id="registroContainer">
+          <div class="registro71"></div>
+        </div>
+        <div class="registro72" id="registroContainer1">
+          <div class="registro73"></div>
+          <b class="buscar">Buscar</b>
+        </div>
+        <div class="registro74" id="registroContainer2">
+          <div class="registro73"></div>
+          <b class="buscar">Checkout</b>
+        </div>
+      </div>
+      <div class="header42">
+        <div class="header43"></div>
+        <img
+          class="rep-logo-1-222"
+          alt=""
+          src="./public/rep-logo-1-2@2x.png"
+          id="rEPLOGO12"
+        />
+      </div>
+      <img class="image-6-icon" alt="" src="./public/image-6@2x.png" />
+
+      <b class="insumos">.INSUMOS</b>
+      <img
+        class="herramientas-parnassa-1"
+        alt=""
+        src="./public/herramientas-parnassa-1@2x.png"
+      />
+
+      <img class="he2-1-icon" alt="" src="./public/he2-1@2x.png" />
+
+      <div class="titulo-trabajos4">
+        <div class="titulo-trabajos-child2"></div>
+        <div class="titulo-trabajos5">
+          <div class="trabajos4">Varios</div>
+        </div>
+      </div>
+      <div class="faq">
+        <div class="faq-child"></div>
+        <div class="hacen-envios">¿Hacen envios?</div>
+        <div class="hacemos-envios-a">
+          Hacemos envios a todo el país con el expreso que quieras
+        </div>
+        <div class="tienen-compra-minima">¿Tienen compra minima?</div>
+        <div class="podes-combinar-entre">
+          Podes combinar entre diferentes articulos. Monto: $50000
+        </div>
+        <div class="cmo-pago">¿Cómo pago?</div>
+        <div class="aceptamos-mercado-pago">
+          Aceptamos Mercado Pago, transferencias y depositos bancarios.
+        </div>
+        <img class="image-7-icon" alt="" src="./public/image-7@2x.png" />
+
+        <img class="image-8-icon" alt="" src="./public/image-8@2x.png" />
+
+        <img class="image-9-icon" alt="" src="./public/image-9@2x.png" />
+      </div>
+    </div>
+  </body>
+</html>
---
+++ secciones.php
@@ -0,0 +1,31 @@
+<?php
+function head(){
+    echo'
+    <div>
+    <img
+        src="./imagenes/rep-logo-1-2@2x.png"
+        id="rEPLOGO12"
+    />
+    </div>';}
+
+    function foot(){
+    echo'
+    <div>
+    <img
+        src="./imagenes/logo-registrado@2x.png"
+        />
+                  
+    <img
+        src="./imagenes/logo-facebook@2x.png"
+        />
+
+    <img
+        src="./imagenes/logo-ig@2x.png" 
+        />
+
+    <img
+        src="./imagenes/logo-twitter@2x.png"
+        />
+    </div>';}
+
+?>
