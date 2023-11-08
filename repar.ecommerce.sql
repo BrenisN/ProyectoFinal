b59f1cf3ef7dff9b416151a3cb1d21073da5bb45
HwaRCala <leo.lviola@gmail.com>
2023-11-08 18:31:24 -0300

    Leo08112023

--- repar.ecommerce.sql
+++ repar.ecommerce.sql
@@ -1,159 +1,72 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-11-2023 a las 00:50:02
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- sql de Repar
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Tabla Productos (Alta, Baja y Modificación)
+-- Columnas IdProducto, nombre, cantidad, precio, por mayor, imagen, categoria, descripcion 
+
+-- Tabla Usuarios
+-- Columnas IdUsuario, sexo, Nombre, edad, email, telefono
+
+-- Tabla Categorias (Alta, Baja y Modificación)
+-- Columnas IdCategoria, nombre
+
+-- tabla Compras (Alta, Baja y Modificación)
+-- Columnas idCompras, idproductos, idusuario
+	
+
+-- Generación de tablas
+
+-- Creo la base de datos repar
+CREATE DATABASE IF NOT EXISTS `repar` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `repar`;
+-- Creo la tabla productos
+CREATE TABLE repar.productos (
+    idProducto int (10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombre varchar(30) NOT NULL,
+    cantidad int (10),
+    precio int (30),
+    porMayor int (30),
+    imagen varchar(30) not null,
+    idcategoria int(30) unsigned not null,
+    descripcion varchar(1000) not null,
+    PRIMARY KEY (idProducto)
+    ); 
+-- Creo la tabla Usuarios
+CREATE TABLE repar.usuarios (
+    idUsuario int (10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    sexo varchar(50) NOT NULL,
+    Nombre varchar(50) not null,
+    email varchar(30) not null,
+    edad int (4) not null,
+    telefono varchar(20) not null,
+    status TINYINT(1) NOT NULL DEFAULT 0,
+    PRIMARY KEY (idUsuario)
+); 
+
+-- Creo la tabla compras
+
+CREATE TABLE repar.compras (
+    idUsuario  INT(10) UNSIGNED NOT NULL,
+    idProducto  INT(10) UNSIGNED NOT NULL,
+    idCompra int (10) UNSIGNED NOT NULL AUTO_INCREMENT,    
+    PRIMARY KEY (idCompra)
+); 
+
+-- Creo tabla Categoria
+CREATE TABLE repar.categorias (
+    idCategoria int (10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombre varchar(50) NOT NULL,
+    PRIMARY KEY (idCategoria)
+); 
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- Asigno la FK dando como referencia a la PK de la otra tabla
 
---
--- Base de datos: `repar`
---
+ALTER TABLE repar.compras
+ADD FOREIGN KEY(idUsuario) REFERENCES repar.usuarios(idUsuario);
 
--- --------------------------------------------------------
+ALTER TABLE repar.compras
+ADD FOREIGN KEY(idProducto) REFERENCES repar.productos(idProducto);
 
---
--- Estructura de tabla para la tabla `categorias`
---
+ALTER TABLE repar.productos
+ADD FOREIGN KEY (idCategoria) REFERENCES repar.Categorias(idCategoria);
 
-CREATE TABLE `categorias` (
-  `idCategoria` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `compras`
---
-
-CREATE TABLE `compras` (
-  `idUsuario` int(10) UNSIGNED NOT NULL,
-  `idProducto` int(10) UNSIGNED NOT NULL,
-  `idCompra` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
-CREATE TABLE `productos` (
-  `idProducto` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `cantidad` int(10) DEFAULT NULL,
-  `precio` int(30) DEFAULT NULL,
-  `porMayor` int(30) DEFAULT NULL,
-  `imagen` varchar(30) NOT NULL,
-  `idcategoria` int(30) UNSIGNED NOT NULL,
-  `descripcion` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `idUsuario` int(10) UNSIGNED NOT NULL,
-  `sexo` varchar(50) NOT NULL,
-  `Nombre` varchar(50) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `edad` int(4) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`idCategoria`);
-
---
--- Indices de la tabla `compras`
---
-ALTER TABLE `compras`
-  ADD PRIMARY KEY (`idCompra`),
-  ADD KEY `idUsuario` (`idUsuario`),
-  ADD KEY `idProducto` (`idProducto`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`idProducto`),
-  ADD KEY `idcategoria` (`idcategoria`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`idUsuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `idCategoria` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `compras`
---
-ALTER TABLE `compras`
-  MODIFY `idCompra` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `idProducto` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `compras`
---
-ALTER TABLE `compras`
-  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`),
-  ADD CONSTRAINT `compras_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`idProducto`);
-
---
--- Filtros para la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`idcategoria`) REFERENCES `categorias` (`idCategoria`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
